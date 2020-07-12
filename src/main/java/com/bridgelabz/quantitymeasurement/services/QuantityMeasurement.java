2727976b8b51bbdb699a9fd233660539c34bedde
nikhildhruwe <nick.dhruwe@gmail.com>
2020-07-13 01:57:43 +0530

    Added: Test class for quantity measurement

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/services/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.quantitymeasurement.services;
+
+import com.bridgelabz.quantitymeasurement.utility.UnitType;
+
+public class QuantityMeasurement {
+
+}
---
+++ src/test/java/QuantityMeasurementTest.java
@@ -0,0 +1,12 @@
+import com.bridgelabz.quantitymeasurement.services.QuantityMeasurement;
+import com.bridgelabz.quantitymeasurement.utility.UnitType;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuantityMeasurementTest {
+
+    @Test
+    public void given12InchAnd1Feet_IfEqual_ShouldReturnTrue() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+    }
+}
