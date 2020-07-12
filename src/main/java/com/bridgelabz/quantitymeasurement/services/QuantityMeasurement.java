c507aa04be604d46a24c643723767bcb9b88191d
nikhildhruwe <nick.dhruwe@gmail.com>
2020-07-13 01:59:34 +0530

    Added: Test case to check inch and feet conversion

--- src/main/java/com/bridgelabz/quantitymeasurement/services/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/services/QuantityMeasurement.java
@@ -3,7 +3,6 @@ package com.bridgelabz.quantitymeasurement.services;
 import com.bridgelabz.quantitymeasurement.utility.UnitType;
 
 public class QuantityMeasurement {
-
     public double calculateUnit(UnitType type, double unit) {
         return unit * type.unitConversion;
     }
